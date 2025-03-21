da9ebba43f6bc10b80aa1414c48d720cb5dbe9b1
code-with-sourav <souravkumarsingh624@gmail.com>
2025-03-21 11:31:11 +0530

    Add the functionality to  download  ISAA test manual

---
+++ AutiCare/Core/Authentication/LearningPageView/Coginitive Games/ColorMatchingGame.swift
@@ -0,0 +1,18 @@
+//
+//  ColorMatchingGame.swift
+//  Auticare
+//
+//  Created by sourav_singh on 20/03/25.
+//
+
+import SwiftUI
+
+struct ColorMatchingGame: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ColorMatchingGame()
+}
---
+++ AutiCare/Core/Authentication/LearningPageView/DocumentInteractionView.swift
@@ -0,0 +1,18 @@
+//
+//  DocumentInteractionView.swift
+//  Auticare
+//
+//  Created by sourav_singh on 21/03/25.
+//
+
+import SwiftUI
+
+struct DocumentInteractionView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    DocumentInteractionView()
+}
