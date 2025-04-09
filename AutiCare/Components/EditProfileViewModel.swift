00c23ddbff413ad54383101613c62b9d68933003
Sudhanshu Kumar <sudhanshukumar07777@gmail.com>
2025-04-09 13:01:09 +0530

    integrated the onboarding screen and now video are fetched from supbase perviously i was playing locally from the app

---
+++ AutiCare/Components/EditProfileViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  EditProfileViewModel.swift
+//  Auticare
+//
+//  Created by Sudhanshu Singh Rajput on 09/04/25.
+//
+
+import Foundation
---
+++ AutiCare/Core/Authentication/ViewModel/SupabaseManager.swift
@@ -0,0 +1,8 @@
+//
+//  SupabaseManager.swift
+//  Auticare
+//
+//  Created by Sudhanshu Singh Rajput on 09/04/25.
+//
+
+import Foundation
