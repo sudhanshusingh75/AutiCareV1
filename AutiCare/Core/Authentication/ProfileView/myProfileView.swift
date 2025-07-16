cb6d13620f9bb5afc8fc54b9675ce7aa333784dc
Sudhanshu Singh <Xethrofer@icloud.com>
2025-07-17 00:10:43 +0530

    made changes in profie view

---
+++ AutiCare/Core/Authentication/ProfileView/myProfileView.swift
@@ -0,0 +1,18 @@
+//
+//  myProfileView.swift
+//  Auticare
+//
+//  Created by Sudhanshu Singh Rajput on 16/07/25.
+//
+
+import SwiftUI
+
+struct myProfileView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    myProfileView()
+}
